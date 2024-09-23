6568817717a65a0be5f04909e3c2866287b9d80e
maxSimmer <maxsimmer@gmail.com>
2024-09-22 21:01:48 -0500

    update intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,7 +5,7 @@ function Intro() {
       <div class="flex items-center justify-center flex-col text-center pt-20 pb-6 ">
          <h1 class='text-4xl md:text-7xl mb-1
          md:mb-3 dark:text-white font-bold'>Maximillian Simmer</h1>
-         <p class="text-base md:text-x1 mb-3 font-medium">Computer Science Student & Full-Stack Engineer</p>
+         <p class="text-base md:text-x1 mb-3 font-medium">Computer Science Student</p>
          <p class="text-sm max-w-xl
          mb-6 font-bold">Currently a fourth-year computer science student at Loyola University Chicago. Plans to go into database
          engineering and to get my masters post graduation. 
