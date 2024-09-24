e163d4db026558b86150fe6e2ef91af0073de25f
maxSimmer <maxsimmer@gmail.com>
2024-09-24 15:35:27 -0500

    update projects layout

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,16 +1,17 @@
  export default [
+
     {
-        title: 'Graphics Based Rock Paper Scissors',
-        imgUrl: '/assets/RPSPortfolioImage.jpg',
-        stack: ['Python'],
-        link: 'https://github.com/maxSimmer/COMP150-Graphics-based-Rock-Paper-Scissors-Python-',
+        title: 'Asteroid Classification Model',
+        imgUrl: '/assets/asteroid ML image.png',
+        stack: ['Python, numpy, pandas, scikit, madplotlib, seaborn'],
+        link: 'https://colab.research.google.com/drive/14BE1Xl8SF7mOW0x0rpPp1nwmS3ZP5VVg?usp=sharing',
     },
 
     {
-        title: 'Portfolio Website',
-        imgUrl: '/assets/portfolioimg.jpg',
-        stack: ['HTML', 'TailwindCSS', 'Javascript',],
-        link: 'https://github.com/maxSimmer/minimalist-portfolio-copy',
+        title: 'CSED Non-Profit Database',
+        imgUrl: '/assets/10-19_ERDiagramUpdated_DoubleLines.png',
+        stack: ['Python, MongoDB, SQLAlchemy'],
+        link: 'https://github.com/maxSimmer/CSED-DBMS-and-ORM-',
     },
 
     {
@@ -21,18 +22,18 @@
     },
 
     {
-        title: 'CSED Database',
-        imgUrl: '/assets/10-19_ERDiagramUpdated_DoubleLines.png',
-        stack: ['Python, MongoDB, SQLAlchemy'],
-        link: 'https://github.com/maxSimmer/CSED-DBMS-and-ORM-',
+        title: 'Portfolio Website',
+        imgUrl: '/assets/portfolioimg.jpg',
+        stack: ['HTML', 'TailwindCSS', 'Javascript',],
+        link: 'https://github.com/maxSimmer/minimalist-portfolio-copy',
     },
 
     {
-        title: 'Asteroid Classification Model',
-        imgUrl: '/assets/asteroid ML image.png',
-        stack: ['Python, numpy, pandas, scikit, madplotlib, seaborn'],
-        link: 'https://colab.research.google.com/drive/14BE1Xl8SF7mOW0x0rpPp1nwmS3ZP5VVg?usp=sharing',
-    },
+        title: 'Graphics Based Rock Paper Scissors',
+        imgUrl: '/assets/RPSPortfolioImage.jpg',
+        stack: ['Python'],
+        link: 'https://github.com/maxSimmer/COMP150-Graphics-based-Rock-Paper-Scissors-Python-',
+    }
 
     
 
